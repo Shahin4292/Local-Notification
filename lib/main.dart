15675f20d24f13be7b595285f484aacbe7c365f4
Shahin4292 <sarker4292@gmail.com>
2025-03-11 12:23:15 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:notification_local_app/service.dart';
-
-import 'home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await NotificationService.instance.initialize();
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // await NotiService().initNotification();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: NotificationScreen(),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:notification_local_app/service.dart';
+//
+// import 'home_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   await NotificationService.instance.initialize();
+//   // WidgetsFlutterBinding.ensureInitialized();
+//   // await Firebase.initializeApp(
+//   //   options: DefaultFirebaseOptions.currentPlatform,
+//   // );
+//   // await NotiService().initNotification();
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//       ),
+//       home: NotificationScreen(),
+//     );
+//   }
+// }
